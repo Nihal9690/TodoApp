783232215f2a7d88004162b5ff7359f725d5861a
FirstRepo <ns87525@gmail.com>
2024-11-23 23:04:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Learn about redux toolkit</h1>
+
+      <AddTodo/>
+      <Todos/>
+    
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
